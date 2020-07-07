bfe6cb472d93c1720787481e42b113bb5b23fb06
nmigacz <nickmigacz@gmail.com>
2020-07-07 13:34:16 -0500

    Personalized:  Auto page reload, style changes

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ let correctColor = Math.floor(Math.random() * 2);
 if(correctColor === 1){
     document.getElementById("first").style.backgroundColor = correctRandomHex;
     document.getElementById("second").style.backgroundColor = incorrectRandomHex;
-    document.getElementById("first").addEventListener("click", function() {alert("Correct!")});
-    document.getElementById("second").addEventListener("click", function() {alert("Incorrect!")});
+    document.getElementById("first").addEventListener("click", function() {alert("Correct!"); location.reload();}); //page automatically reloads if answer is correct, same for else statement
+    document.getElementById("second").addEventListener("click", function() {alert("Incorrect! Try Again")});
 } else{
     document.getElementById("second").style.backgroundColor = correctRandomHex;
     document.getElementById("first").style.backgroundColor = incorrectRandomHex;
-    document.getElementById("second").addEventListener("click", function() {alert("Correct!")});
-    document.getElementById("first").addEventListener("click", function() {alert("Incorrect!")});
+    document.getElementById("second").addEventListener("click", function() {alert("Correct!"); location.reload();});
+    document.getElementById("first").addEventListener("click", function() {alert("Incorrect! Try Again")});
 }
