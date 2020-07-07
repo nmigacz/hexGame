357b3739040326ee46ff39a2057b2cb10fb696ce
nmigacz <nickmigacz@gmail.com>
2020-07-07 12:46:15 -0500

    Functioning small hex game, from tutorial

---
+++ main.js
@@ -0,0 +1,18 @@
+correctRandomHex = "#" + Math.floor(Math.random() * 16777215).toString(16);
+incorrectRandomHex = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+document.getElementById("hexNum").innerHTML = correctRandomHex;
+
+let correctColor = Math.floor(Math.random() * 2);
+
+if(correctColor === 1){
+    document.getElementById("first").style.backgroundColor = correctRandomHex;
+    document.getElementById("second").style.backgroundColor = incorrectRandomHex;
+    document.getElementById("first").addEventListener("click", function() {alert("Correct!")});
+    document.getElementById("second").addEventListener("click", function() {alert("Incorrect!")});
+} else{
+    document.getElementById("second").style.backgroundColor = correctRandomHex;
+    document.getElementById("first").style.backgroundColor = incorrectRandomHex;
+    document.getElementById("second").addEventListener("click", function() {alert("Correct!")});
+    document.getElementById("first").addEventListener("click", function() {alert("Incorrect!")});
+}
